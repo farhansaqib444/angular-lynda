3adbf8a9fc52359c1280f35eb75711561e688bf3
Farhan <farhansaqib444@outlook.com>
2019-12-10 09:35:05 +0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { MediaItemComponent } from './media-item/media-item.component';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component';
+import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent,
+    FavoriteDirective,
+    CategoryListPipe,
+    MediaItemFormComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {}
---
+++ src/app/media-item-form/media-item-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'mw-media-item-form',
+  templateUrl: './media-item-form.component.html',
+  styleUrls: ['./media-item-form.component.css']
+})
+export class MediaItemFormComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(){}
+  onSubmit(mediaItem){
+    console.log(mediaItem);
+  }
+}
